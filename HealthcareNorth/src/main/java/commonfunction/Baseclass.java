eb47990333ea79c7dcef07eec72fa02a393d8076
vigne <vigne@LAPTOP-II6A6BGH.lan>
2020-10-05 16:12:57 -0400

    Changed CloseBrowser

--- HealthcareNorth/src/main/java/commonfunction/Baseclass.java
+++ HealthcareNorth/src/main/java/commonfunction/Baseclass.java
@@ -9,9 +9,11 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -99,8 +101,8 @@ public class Baseclass {
 	 @AfterSuite
 	 public void closebrowser() {
 		 
-	// WebElement teardown=driver.findElement(By.linkText("Logout"));
-		//teardown.click();
+	 WebElement teardown=driver.findElement(By.linkText("Logout"));
+		teardown.click();
 		
 		 
 	 }
